a5771218c701bf05d69d414c4516ce2d26ecd843
Stefan Chrisostomus <stefanchrisostomus13@gmail.com>
2021-06-08 08:13:30 +0200

    Add i3 config to larbs

--- larbs.sh
+++ larbs.sh
@@ -14,8 +14,8 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/stefanchrisostomus/voidrice-i3.git"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/stefanchrisostomus/LARBS/master/larbs.sh"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
 
