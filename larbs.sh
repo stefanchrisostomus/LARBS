ef23e84e95ca3f4278e99ad28647e268e8e3d772
Stefan Chrisostomus <stefan.chrisostomus@outlook.com>
2021-08-24 23:26:15 +0200

    Update larbs.sh

--- larbs.sh
+++ larbs.sh
@@ -200,8 +200,8 @@ manualinstall $aurhelper || error "Failed to install AUR helper."
 # and all build dependencies are installed.
 installationloop
 
-dialog --title "LARBS Installation" --infobox "Finally, installing \`libxft-bgra\` to enable color emoji in suckless software without crashes." 5 70
-yes | sudo -u "$name" $aurhelper -S libxft-bgra-git >/dev/null 2>&1
+dialog --title "LARBS Installation" --infobox "Finally, installing \`libxft\` to enable color emoji in suckless software without crashes." 5 70
+yes | sudo -u "$name" $aurhelper -S libxft >/dev/null 2>&1
 
 # Install the dotfiles in the user's home directory
 putgitrepo "$dotfilesrepo" "/home/$name" "$repobranch"
